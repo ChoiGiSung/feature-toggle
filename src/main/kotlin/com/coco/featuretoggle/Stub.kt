eaa68d5efb383a10d31159753b502768de5ba7f6
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2024-06-24 23:36:04 +0900

    add CanaryToggleConfiguration

--- src/main/kotlin/com/coco/featuretoggle/Stub.kt
+++ src/main/kotlin/com/coco/featuretoggle/Stub.kt
@@ -4,8 +4,8 @@ object Stub {
     const val TRUE_FEATURE = "sample-feature-true"
     const val FALSE_FEATURE = "sample-feature-false"
 
-    class ToggleRepository{
-        fun findAll(): List<ToggleConfiguration>{
+    class ToggleRepository {
+        fun findAll(): List<ToggleConfiguration> {
             return listOf(
                 ToggleConfiguration(TRUE_FEATURE, true),
                 ToggleConfiguration(FALSE_FEATURE, false)
@@ -13,9 +13,17 @@ object Stub {
         }
     }
 
-    class ToggleConfiguration(
-        val key: String,
-        val value: Boolean
+    open class ToggleConfiguration(
+        open val key: String,
+        open val value: Boolean
     )
+
+    class CanaryToggleConfiguration(
+        override val key: String,
+        override val value: Boolean,
+        val debug: Boolean,
+        val permittedUserIds: Set<Long> = emptySet(),
+        val canaryUserPercentage: Int// 0 to 100
+    ) : ToggleConfiguration(key, value)
 
 }
