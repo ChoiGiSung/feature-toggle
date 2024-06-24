02fabb46abb576c4293d31efb6ce9891f55ae5a4
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2024-06-24 22:26:58 +0900

    init

---
+++ src/main/kotlin/com/coco/featuretoggle/FeaturetoggleApplication.kt
@@ -0,0 +1,11 @@
+package com.coco.featuretoggle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FeaturetoggleApplication
+
+fun main(args: Array<String>) {
+	runApplication<FeaturetoggleApplication>(*args)
+}
---
+++ src/test/kotlin/com/coco/featuretoggle/FeaturetoggleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coco.featuretoggle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FeaturetoggleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
